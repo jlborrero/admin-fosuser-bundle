738091c0f079713f5492eda1d4c3f9ade537e950
Joe Luis Borrero Senciales <jlborrero@gmail.com>
2013-09-05 21:08:52 -0400

    nuevos cambios de adaptacion al bt2

--- Entity/User.php
+++ Entity/User.php
@@ -1,14 +1,14 @@
 <?php
 namespace Jlbs\AdminFOSUserBundle\Entity;
 
-use FOS\UserBundle\Entity\User as BaseUser,
+use FOS\UserBundle\Model\User as AbstractUser,
     Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="user")
  */
-class User extends BaseUser
+class User extends AbstractUser
 {
     /**
      * @ORM\Id
